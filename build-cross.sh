9b6be7a083243650a5ef2d9735011947102431d5
minnyres <liuthgw23@gmail.com>
2024-12-23 13:05:13 +0800

    build.sh

---
+++ build-cross.sh
@@ -0,0 +1,69 @@
+#!/bin/bash -e
+
+
+
+prefix_dir=$PWD/mpv-depends
+mkdir -p "$prefix_dir"
+[ -z "$vcpkg_dir" ] && vcpkg_dir=$PWD/vcpkg
+vcpkg_libs_dir=$vcpkg_dir/installed/arm64-mingw-dynamic
+[ -z "$llvm_dir" ] && llvm_dir=$PWD/llvm-mingw
+
+wget="wget -nc --progress=bar:force"
+gitclone="git clone --depth=1 --recursive"
+
+mpfr_ver=4.2.1
+
+export PATH=$llvm_dir/bin:$PATH
+export TARGET=aarch64-w64-mingw32
+export CC=$TARGET-gcc
+export CXX=$TARGET-g++
+export AR=$TARGET-ar
+export NM=$TARGET-nm
+export RANLIB=$TARGET-ranlib
+export STRIP=$TARGET-strip
+
+export CFLAGS="-O2 -I$prefix_dir/include -I$vcpkg_libs_dir/include"
+export CXXFLAGS=$CFLAGS
+export CPPFLAGS="-I$prefix_dir/include -I$vcpkg_libs_dir/include"
+export LDFLAGS="-s -L$prefix_dir/lib -L$vcpkg_libs_dir/lib"
+
+# anything that uses pkg-config
+export PKG_CONFIG=/usr/bin/pkg-config
+export PKG_CONFIG_LIBDIR="$prefix_dir/lib/pkgconfig:$vcpkg_libs_dir/lib/pkgconfig"
+export PKG_CONFIG_PATH=$PKG_CONFIG_LIBDIR
+
+# autotools(-like)
+commonflags="--prefix=$prefix_dir --host=$TARGET --enable-static=no --enable-shared"
+
+# CMake
+cmake_args=(
+    -G "Ninja" -B "build"
+    -Wno-dev
+    -DCMAKE_SYSTEM_NAME=Windows
+    -DCMAKE_FIND_ROOT_PATH="$prefix_dir;$vcpkg_libs_dir"
+    -DCMAKE_RC_COMPILER="${TARGET}-windres"
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_INSTALL_PREFIX=$prefix_dir
+)
+
+function cmakeplusinstall {
+    cmake --build build
+    cmake --install build
+}
+
+function gnumakeplusinstall {
+    make -j $(nproc)
+    make install
+}
+
+mkdir -p src
+mkdir -p $prefix_dir/lib/pkgconfig/ 
+cd src
+
+# mpfr
+[ -d mpfr-$mpfr_ver ] || $wget https://www.mpfr.org/mpfr-current/mpfr-$mpfr_ver.tar.xz
+tar xf mpfr-$mpfr_ver.tar.xz
+pushd mpfr-$mpfr_ver
+./configure $commonflags 
+gnumakeplusinstall
+popd
